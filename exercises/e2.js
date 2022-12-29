ab1263680e59ac7f9017eb91deb8c4ab66164b42
deppravacion <deppravacion@gmail.com>
2022-12-29 16:48:54 -0700

    no quarter

--- exercises/e2.js
+++ exercises/e2.js
@@ -6,6 +6,9 @@ import { data } from "../data/data";
 
 export function getAsteroidNames(data) {
   // Your code goes here...
+  const asteroids = data.asteroids;
+  const asteroidNames = asteroids.map(roid => roid.name)
+  return asteroidNames;
 }
 
 
